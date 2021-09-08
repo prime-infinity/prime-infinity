58e2e9555594a444c104747cbb18025c49e55c7b
prime-infinity <soe.a.prime@gmail.com>
2021-09-09 00:56:00 +0100

    deploy

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'new deployment'
+git push -f git@github.com:prime-infinity/prime-infinity.git main:gh-pages
+
+cd -
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    publicPath: '/prime-infinity/'
+}
